d1b593fa9846ef427e03bc255da86432673fda92
Lynch <l.wu@student.tue.nl>
2022-06-10 17:13:07 +0200

    add file

--- Assignment4/Code - for students/ISTA.py
+++ Assignment4/Code - for students/ISTA.py
@@ -7,35 +7,35 @@ class ISTAI_main(object):
     def __init__(self):
         # ,mu,shrinkage,K,y
         super(ISTAI_main, self).__init__()
-        # self.mu = mu                # Step size
-        # self.shrinkage = shrinkage  # shrinkage parameters 
-        # self.K = K                  # Interation
-        # self.y=y
 
-    
-    def softthreshold(self,x,shrinkage):        
-        # torch.sign(x) Returns a new tensor with the signs of the elements of input.
-        return torch.multiply(torch.sign(x) ,torch.maximum(torch.abs(x) - shrinkage, torch.tensor(0.)))
-
-
+    def softthreshold(self,x,theta):
+        """softthreshold, x - input, theta - shrinkage parameters: mu*lambda"""      
+        return torch.multiply(torch.sign(x) ,torch.maximum(torch.abs(x) - theta, torch.tensor(0.)))
 
     def ISTA(self,mu,lmbd,K,y:torch.tensor)->torch.tensor:
         """
-        y = A  # input each batch
+        # input 
+        - y  = input each batch \\
+        - mu = constant - step size \\
+        - lmbd = Lambda - Lipschitz continuous \\
+        - K - max iteration \\
         """        
-        # 32,32
+        # [32,32]
         A = torch.diag_embed(torch.flatten(y)) # Diagnoal matrix - diagonal length = 1024
         x = torch.zeros(len(torch.flatten(y))) # 1024 zero
         error = []
+
         for i in range(K):
-            g = A.T@(torch.flatten(y)-A@x) 
-            x_2 = x +mu*g
-            x_hat = self.softthreshold(x_2, mu*lmbd)            
-            error = torch.nn.functional.mse_loss(x,x_hat)
-            if error < 1e-15:
+            # g = torch.transpose(A,0,1)@(torch.flatten(y)-A@x) 
+            # torch.transpose(A,0,1)
+            x_2 = x +mu*A.T@(torch.flatten(y)-A@x) # mu = 1/L
+            shrinkage = mu*lmbd
+            x_hat = self.softthreshold(x_2, shrinkage)
+            error = torch.nn.functional.mse_loss(x_hat,x)
+            if error < 1e-7:
                 break
             else:
-                x = x_hat
+                x = x_hat   
 
         return torch.tensor(x_hat)
 
